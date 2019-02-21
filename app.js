e385d673c33806295a3d5c87c403aa028d59ab7e
kele <chj.ymh@gmail.com>
2019-02-21 22:14:46 +0800

    整理

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
  * @Author: kele 
  * @Date: 2019-01-11 15:30:25 
  * @Last Modified by: kele
- * @Last Modified time: 2019-01-27 11:06:19
+ * @Last Modified time: 2019-02-21 22:14:32
  */
 const koa = require('koa');
 const koaStatic = require('koa-static');
 const koaCors = require('@koa/cors');
 const koaLogger = require('koa-logger');
 const setConfig = require('./config/config.controller').setConfig;
-// const test = require('./tools/crawler/test')
+const record = require('./tools/record/err_log');
 
 const app = new koa();
 // 配置静态文件
@@ -20,5 +20,13 @@ if (setConfig.ALLOW_ORIGIN) {
 }
 // 配置日志
 app.use(koaLogger());
+app.use(record);
+
+app.use(async (ctx, next) => {
+	ctx.body = 'hello';
+});
+app.on('error', (err) => {
+	console.error('server error', err);
+});
 
 app.listen(setConfig.PORT);
