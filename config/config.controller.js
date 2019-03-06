02a1533051add0945eef158ec413b45b3a64fee0
kele <chj.ymh@gmail.com>
2019-03-06 17:22:28 +0800

    备份

--- config/config.controller.js
+++ config/config.controller.js
@@ -2,14 +2,16 @@
  * @Author: kele 
  * @Date: 2019-01-11 15:30:58 
  * @Last Modified by: kele
- * @Last Modified time: 2019-03-01 13:10:45
+ * @Last Modified time: 2019-03-02 14:12:46
  */
 const fs = require('fs');
 const yamljs = require('yamljs');
 
 
 //线上dev，本地local
-let config_controller = process.env.NODE_ENV === 'production' ? 'dev' : 'local';
+let config_controller = {
+	USE_CONFIG: process.env.NODE_ENV === 'production' ? 'dev' : 'local'
+}
 let setConfig = yamljs.parse(
 	fs.readFileSync(__dirname + '/setting/' + config_controller.USE_CONFIG + '.yml').toString()
 );
--- tools/task_manager/task_controller.js
+++ tools/task_manager/task_controller.js
@@ -2,10 +2,10 @@ const Task = require('./Task')
 const morning = require('./list/morning')
 
 exports.morning = () => {
-    return new Task('Good Morning', '10 10 9 * * * *', morning)
+    return new Task('Good Morning', '50 59 8 * * * *', morning)
 }
 
 
 exports.running = () => {
-    new Task('Good Morning', '10 10 9 * * * *', morning).start()
+    new Task('Good Morning', '50 59 8 * * * *', morning).start()
 }
